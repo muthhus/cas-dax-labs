fc2d34f566558e4ebc6e77a8b5441f8cd7d77455
Kerzner,Mark <c_kerzner.mark@cerner.com>
2013-09-24 23:07:57 -0500

    tests

---
+++ generators/src/test/java/com/datastax/video/GenerateVideoEventsTest.java
@@ -0,0 +1,48 @@
+package com.datastax.video;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mark Kerzner
+ */
+public class GenerateVideoEventsTest {
+    
+    public GenerateVideoEventsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class GenerateVideoEvents.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] argv = new String[2];
+        argv[0] = "2";
+        argv[1] = "4";
+        GenerateVideoEvents.main(argv);
+        // TODO check that the expected files exists and has the expected number of records
+        assertTrue(true);
+    }
+}
